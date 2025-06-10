3c6dea71dd0448c64e528ddd1db3e7fa09dadc2f
tubi55 <tubi55@nate.com>
2025-06-10 10:42:19 +0900

    atoms: Button

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,6 +1,8 @@
 import { twMerge } from "tailwind-merge";
 
+//props (className: 버튼 스타일, children: 버튼 내용, onClick: 클릭 이벤트 핸들러)
 export default function Button({ className, children, onClick }) {
+  //스타일 변수
   const btnStyle = `
     inline-block px-6 py-2 rounded bg-theme3    
     text-sm font-semibold text-white    
@@ -10,6 +12,7 @@ export default function Button({ className, children, onClick }) {
   `;
 
   return (
+    //스타일 적용 및 이벤트 핸들러 연결
     <button className={twMerge(btnStyle, className)} onClick={onClick}>
       {children}
     </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import Button from "../components/atoms/Button";
+
 export default function Home() {
   return (
-    <section className="w-full h-screen overflow-y-auto">
-      <div className="h-[300vh]"></div>
+    <section className="p-20 flex gap-10">
+      <Button>button1</Button>
+      <Button className="bg-black shadow-black/30">button2</Button>
+      <Button onClick={() => alert("Clicked!!")}>button3</Button>
     </section>
   );
 }
