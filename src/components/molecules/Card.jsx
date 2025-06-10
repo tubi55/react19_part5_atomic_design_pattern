e5e06908f50af6dd25057a30ced0cf5b023c0ff6
tubi55 <tubi55@nate.com>
2025-06-10 16:16:55 +0900

    molecule: Card

---
+++ src/components/molecules/Card.jsx
@@ -0,0 +1,26 @@
+import Text from "../atoms/Text";
+import Pic from "../atoms/Pic";
+import Rating from "../atoms/Rating";
+import { twMerge } from "tailwind-merge";
+
+export default function Card({
+  data, // 카드에 표시할 콘텐츠 객체 (id, title, poster_path, vote_average)
+  className, // 외부에서 전달되는 추가 스타일 클래스
+}) {
+  return (
+    <article className={twMerge("w-full theme-text", className)}>
+      <Pic
+        className="h-[22vw] max-2xl:h-[30vw] max-xl:h-[40vw] max-md:h-[50vw] max-sm:h-[110vw]"
+        title={data.title}
+        url={`/${data.id}`}
+        src={data.poster_path}
+        shadow
+        rounded
+      />
+      <Text tag="h2" className="text-3xl font-dongle mt-3" url={`/${data.id}`}>
+        {data.title}
+      </Text>
+      <Rating value={data.vote_average} total={10} rates={5} />
+    </article>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import Logo from "../components/molecules/Logo";
+import Card from "../components/molecules/Card";
 
 export default function Home() {
+  const data = {
+    id: 1,
+    title: "베이워치: SOS 해상 구조대",
+    poster_path:
+      "https://image.tmdb.org/t/p/w342/6HE4xd8zloDqmjMZuhUCCw2UcY1.jpg",
+    vote_average: 7.8,
+  };
+
   return (
-    <header className="p-20">
-      <Logo title="DCODELAB" desc="UI/UX DEVELOPMENT" />
-    </header>
+    <section className="p-20">
+      <Card data={data} />
+    </section>
   );
 }
