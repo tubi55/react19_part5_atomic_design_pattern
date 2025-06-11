93eebfd5f71f1b9de0e5ced5ec3a0ff4b42ee648
tubi55 <tubi55@nate.com>
2025-06-11 09:28:43 +0900

    molecule: List

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,25 @@
-import Bg from "../components/atoms/Bg";
-import Bar from "../components/molecules/Bar";
+import Text from "../components/atoms/Text";
+import List from "../components/molecules/List";
 
 export default function Home() {
-  const movieData = {
-    genres: [
-      { id: 16, name: "애니메이션" },
-      { id: 878, name: "판타지" },
-      { id: 28, name: "액션" },
-    ],
-    release_date: "2025-06-05",
-    runtime: 85,
-    vote_average: 9.4,
-  };
+  const data = [
+    { text: "review1", date: "2024.06.11" },
+    { text: "review2", date: "2024.06.12" },
+    { text: "review3", date: "2024.06.13" },
+  ];
 
   return (
-    <section className="w-full h-screen flex-center">
-      <Bg src="https://image.tmdb.org/t/p/w1280/7HqLLVjdjhXS0Qoz1SgZofhkIpE.jpg" />
-
-      <div className="flex-wrap gap-2 font-noto text-xs font-bold mb-5">
-        <Bar arr={movieData.genres.map((el) => el.name)} />
-        <Bar>{movieData.release_date.split("-").join(".")}</Bar>
-        <Bar>{movieData.runtime}분</Bar>
-        <Bar>{movieData.vote_average} / 10</Bar>
-      </div>
+    <section className="p-20">
+      <List data={data} className="border p-3 mb-2">
+        {(el) => (
+          <>
+            <Text tag="h2" className="text-2xl font-bold mb-1">
+              {el.text}
+            </Text>
+            <Text>{el.date}</Text>
+          </>
+        )}
+      </List>
     </section>
   );
 }
